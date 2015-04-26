56af678cec8790147f03f8ac8ba9478116b2d84c
ronimour <ronimour@gmail.com>
2015-04-26 13:11:59 -0300

    inclusao das libs e primeiros experimentos com a api do github.eclipse

---
+++ MiningRepository/src/br/ufrn/repositoyMining/main/Teste.java
@@ -0,0 +1,45 @@
+package br.ufrn.repositoyMining.main;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.RepositoryCommit;
+import org.eclipse.egit.github.core.RepositoryId;
+import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.service.CommitService;
+import org.eclipse.egit.github.core.service.RepositoryService;
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+		try {
+			GitHubClient client = new GitHubClient();
+			client.setOAuth2Token("9e9ed0cd365691a55ab3c63ad8a86447f3560d9e");
+			
+			
+			CommitService cservice = new CommitService(client);
+			
+			List<RepositoryCommit> rc = cservice.getCommits(RepositoryId
+					.create("spring-projects", "spring-boot"));
+			if (!rc.isEmpty()) {
+				for (Iterator iterator = rc.iterator(); iterator.hasNext();) {
+					RepositoryCommit repositoryCommit = (RepositoryCommit) iterator
+							.next();
+					System.out.println(repositoryCommit.getStats().getTotal());
+
+				}
+			} else {
+				System.out.println("lista vazia");
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
